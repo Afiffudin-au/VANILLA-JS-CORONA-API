96499ce4a73720527642154cd3d1b40fd66d65f1
Afiffudin-au <afiffudin077@gmail.com>
2020-06-22 22:11:25 +0700

    make the code easy to read

--- script.js
+++ script.js
@@ -4,20 +4,36 @@ const deathCases = document.querySelector('.death-cases');
 const dataTable = document.querySelector('.data-table-body');
 const changeSort = document.querySelector('.sort');
 let dataFixs;
-fetch('https://api.covid19api.com/world/total')
+
+//--------------------------------------------
+//get data world (3 summary)
+function getWorldData(){
+  return fetch('https://api.covid19api.com/world/total')
   .then((result) => result.json())
-  .then((result) => {
-    totalCases.innerHTML = numberWithCommas(result.TotalConfirmed);
-    recoveryCases.innerHTML = numberWithCommas(result.TotalRecovered);
-    deathCases.innerHTML = numberWithCommas(result.TotalDeaths);
-    fetch('https://api.covid19api.com/summary')
-      .then((result) => result.json())
-      .then((result) => {
-        dataFixs = result.Countries;
-        console.log(dataFixs);
-        dataTable.innerHTML = getDataTable(dataFixs);
-      });
-  });
+  .then((result) => result);
+}
+//display world data (3 summary)
+async function displayWorldData(){
+  const result = await getWorldData();
+  totalCases.innerHTML = numberWithCommas(result.TotalConfirmed);
+  recoveryCases.innerHTML = numberWithCommas(result.TotalRecovered);
+  deathCases.innerHTML = numberWithCommas(result.TotalDeaths);
+}
+
+// ---------------------------------------------
+//get summary data world almost all
+function getSummaryData(){
+  return fetch('https://api.covid19api.com/summary')
+  .then((result) => result.json())
+  .then((result) => result);
+}
+//display summary data almost all
+async function displaySummaryData(){
+  const dataSummary = await getSummaryData();
+  dataFixs = dataSummary.Countries;
+  dataTable.innerHTML = getDataTable(dataFixs);
+}
+
 
 function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
@@ -45,3 +61,5 @@ function handleSort() {
   dataTable.innerHTML = getDataTable(dataFixs);
 }
 changeSort.addEventListener('change', handleSort);
+window.addEventListener('load',displayWorldData);
+window.addEventListener('load',displaySummaryData);
