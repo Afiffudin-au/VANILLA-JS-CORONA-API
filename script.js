1c437b7554d95dc4b0888ae08c5f0087c5f05ca7
Afiffudin-au <afiffudin077@gmail.com>
2020-06-23 19:31:05 +0700

    Create Sort By Country and Create Search Table

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const recoveryCases = document.querySelector('.recovery-cases');
 const deathCases = document.querySelector('.death-cases');
 const dataTable = document.querySelector('.data-table-body');
 const changeSort = document.querySelector('.sort');
+const changeSortByCountry = document.querySelector('.sort-alphabet');
+const tableInputSearch = document.querySelector('#tableSearch');
 let dataFixs;
 
 //--------------------------------------------
@@ -58,8 +60,9 @@ function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 function getDataTable(dataFixs) {
-  return dataFixs.map((dataFix, i) => {
-    return ` <tr>
+  return dataFixs
+    .map((dataFix, i) => {
+      return ` <tr>
         <th scope="row">${i + 1}</th>
         <td>${dataFix.Country}</td>
         <td>${numberWithCommas(dataFix.TotalConfirmed)}</td>
@@ -68,7 +71,8 @@ function getDataTable(dataFixs) {
         <td>${numberWithCommas(dataFix.TotalRecovered)}</td>
         <td>${dataFix.Date}</td>
       </tr>`;
-  });
+    })
+    .join('');
 }
 function handleSort() {
   if (this.value == 'sortlargest') {
@@ -79,6 +83,35 @@ function handleSort() {
   }
   dataTable.innerHTML = getDataTable(dataFixs);
 }
+function handleSortByCountry() {
+  if (this.value == 'a_z') {
+    dataFixs.sort((a, b) => (a.Country > b.Country ? 1 : -1));
+  }
+  if (this.value == 'z_a') {
+    dataFixs.sort((a, b) => (a.Country < b.Country ? 1 : -1));
+  }
+  dataTable.innerHTML = getDataTable(dataFixs);
+}
+
+function handleSearch() {
+  let txtValue;
+  const filter = this.value.toUpperCase();
+  const table = document.querySelector('.data-table-body');
+  const trs = table.querySelectorAll('tr');
+  trs.forEach((tr) => {
+    const td = tr.querySelectorAll('td')[0]; // take the index td always to 0 = country
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr.style.display = '';
+      } else {
+        tr.style.display = 'none';
+      }
+    }
+  });
+}
 changeSort.addEventListener('change', handleSort);
+changeSortByCountry.addEventListener('change', handleSortByCountry);
+tableInputSearch.addEventListener('keyup', handleSearch);
 window.addEventListener('load', displayWorldData);
 window.addEventListener('load', displaySummaryData);
